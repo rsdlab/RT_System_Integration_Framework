cf830fd14b5284a031811bfe24e4d341c0b44150
KatoMisa <112141989+KatoMisa@users.noreply.github.com>
2023-12-05 10:23:54 +0900

    Delete system directory

--- system/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-ross=`ps -e -o pid,cmd | grep rosmaster | grep -v grep | awk '{ print $1 }'`
-kill $ross
-rts=`ps -e -o pid,cmd | grep ./mgr.py | grep -v grep | awk '{ print $1 }'`
-kill $rts
