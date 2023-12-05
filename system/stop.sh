04c47f96ab93c3df709b10f455294bcf333e27aa
KatoMisa <112141989+KatoMisa@users.noreply.github.com>
2023-12-05 10:34:50 +0900

    Update stop.sh

--- system/stop.sh
+++ system/stop.sh
@@ -4,3 +4,5 @@ ross=`ps -e -o pid,cmd | grep rosmaster | grep -v grep | awk '{ print $1 }'`
 kill $ross
 rts=`ps -e -o pid,cmd | grep ./mgr.py | grep -v grep | awk '{ print $1 }'`
 kill $rts
+rts=`ps -e -o pid,cmd | grep rtcd2_python3  | grep -v grep | awk '{ print $1 }'`
+kill $rts
